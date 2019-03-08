a53276821cfb8303788ac431f8e9b1340fa03e86
adminlopez <a.lopezc@hotmail.com>
2019-03-08 14:17:37 -0600

    proyecto nuevo y limpio

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,23 +8,3 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 }
-
-namespace Laraveles\Spanish\Tests;
-
-use Laraveles\Spanish\SpanishServiceProvider;
-use Orchestra\Testbench\TestCase as Orchestra;
-
-abstract class TestCase extends Orchestra
-{
-    /**
-     * @param \Illuminate\Foundation\Application $app
-     *
-     * @return array
-     */
-    protected function getPackageProviders($app)
-    {
-        return [
-            SpanishServiceProvider::class,
-        ];
-    }
-}
